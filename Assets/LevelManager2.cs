4a4710f73a1b8bb6dc26a0c3f5e50a5ff48572f2
boriest <boriest@gmail.com>
2016-03-17 14:38:00 +0100

    selection

---
+++ Assets/LevelManager2.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager2 : MonoBehaviour {
+    public void LoadScene(string name) {
+        Application.LoadLevel(name);
+    }
+}
